d2263d8aeaa96af572ba18d67a5a22d72d2dfc3a
Oscar Nieto <a01705090@itesm.mx>
2023-08-20 22:52:32 -0600

    Importing relevant data

--- Module 2/LearningImplementation.py
+++ Module 2/LearningImplementation.py
@@ -1,9 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv(r'dataset.csv')
+x = dataset.iloc[:,[2,3]].values #Age, Estimated Salary
+y = dataset.iloc[:, 4].values #Purchased
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
+print(x_train)
 
 class LogisticRegression:
-    def __init__(self, learning_rate=0.01, num_iterations=1000):
+    def __init__(self, learning_rate=0.01, epochs=1000):
         self.learning_rate = learning_rate
-        self.num_iterations = num_iterations
+        self.epochs = epochs
         self.weights = None
         self.bias = None
 
@@ -15,7 +25,7 @@ class LogisticRegression:
         self.weights = np.zeros(num_features)
         self.bias = 0
 
-        for _ in range(self.num_iterations):
+        for _ in range(self.epochs):
             linear_model = np.dot(X, self.weights) + self.bias
             predictions = self.sigmoid(linear_model)
 
@@ -36,7 +46,7 @@ class LogisticRegression:
 X = np.array([[2.5, 3.5], [1.5, 2.5], [3.5, 4.5], [1.0, 1.0]])
 y = np.array([1, 0, 1, 0])
 
-model = LogisticRegression(learning_rate=0.01, num_iterations=1000)
+model = LogisticRegression(learning_rate=0.01, epochs=1000)
 model.fit(X, y)
 
 test_data = np.array([[2.0, 3.0], [1.0, 2.0]])
---
+++ Module 2/readme.md
@@ -0,0 +1,7 @@
+# To run this, you need to install:
+python -m pip install --upgrade pip
+pip install numpy, pandas, matplotlib, scikit-learn
+
+**The use of scikit-learn its just to split the dataset**
+
+**To run: ** python3 .\LearningImplementation.py
