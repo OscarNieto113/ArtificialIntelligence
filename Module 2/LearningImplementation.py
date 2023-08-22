01d374d2590bafd46a0ab8e328493a9910790572
Oscar Nieto <a01705090@itesm.mx>
2023-08-21 21:01:57 -0600

    First iteration

--- Module 2/LearningImplementation.py
+++ Module 2/LearningImplementation.py
@@ -2,53 +2,84 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()# Create a StandardScaler object
 
 dataset = pd.read_csv(r'dataset.csv')
 x = dataset.iloc[:,[2,3]].values #Age, Estimated Salary
 y = dataset.iloc[:, 4].values #Purchased
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
-print(x_train)
+x_scaled = scaler.fit_transform(x) # Fit and transform the training data
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=0)
+
+def confusion_matrix(predictions, actual):
+    conf_matrix = np.zeros((2, 2))
+    for i in range(len(predictions)):
+        conf_matrix[predictions[i]][actual[i]] += 1
+    return conf_matrix
 
 class LogisticRegression:
-    def __init__(self, learning_rate=0.01, epochs=1000):
+    def __init__(self, learning_rate, epochs):
         self.learning_rate = learning_rate
         self.epochs = epochs
-        self.weights = None
-        self.bias = None
+        self.weights = np.random.rand
+        self.threshold = 0.5
 
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
+    
+    def cost_function(self, y, y_pred):
+        m = len(y)
+        cost = (-1 / m) * np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
+        return cost
+    
+    def gradient_descent(self, x, y, learning_rate, epochs):
+        m, n = x.shape
+        theta = np.zeros(n)
+        costs = []
 
-    def fit(self, X, y):
-        num_samples, num_features = X.shape
-        self.weights = np.zeros(num_features)
-        self.bias = 0
+        for _ in range(epochs):
+            z = np.dot(x, theta)
+            y_pred = self.sigmoid(z)
+            gradient = np.dot(X.T, (y_pred - y)) / m
+            theta -= learning_rate * gradient
+            cost = self.cost_function(y, y_pred)
+            costs.append(cost)
+
+        return theta, costs
+
+    def fit(self, x, y):
+        m, n = x.shape
+        self.theta = np.random.rand(n)
+        self.costs = []
 
         for _ in range(self.epochs):
-            linear_model = np.dot(X, self.weights) + self.bias
-            predictions = self.sigmoid(linear_model)
+            z = np.dot(x, self.theta)
+            y_pred = self.sigmoid(z)
+            gradient = np.dot(x.T, (y_pred - y)) / m
+            self.theta -= self.learning_rate * gradient
+            cost = self.cost_function(y, y_pred)
+            self.costs.append(cost)
 
-            # Gradient descent
-            dw = (1/num_samples) * np.dot(X.T, (predictions - y))
-            db = (1/num_samples) * np.sum(predictions - y)
+    def predict(self, x):
+        y_pred = self.sigmoid(np.dot(x, self.theta))
+        return (y_pred >= self.threshold).astype(int)
 
-            self.weights -= self.learning_rate * dw
-            self.bias -= self.learning_rate * db
+# Create and train the logistic regression model
+model = LogisticRegression(learning_rate=0.001, epochs=1000)
+model.fit(x_train, y_train)
 
-    def predict(self, X):
-        linear_model = np.dot(X, self.weights) + self.bias
-        predictions = self.sigmoid(linear_model)
-        class_predictions = [1 if p > 0.5 else 0 for p in predictions]
-        return class_predictions
+# Make predictions
+prediction = model.predict(x_test)
+print("Predicted class: \n", prediction)
+print("Real values: \n", y_test)
 
-# Example usage
-X = np.array([[2.5, 3.5], [1.5, 2.5], [3.5, 4.5], [1.0, 1.0]])
-y = np.array([1, 0, 1, 0])
-
-model = LogisticRegression(learning_rate=0.01, epochs=1000)
-model.fit(X, y)
-
-test_data = np.array([[2.0, 3.0], [1.0, 2.0]])
-predictions = model.predict(test_data)
-print(predictions)
+tn, fp, fn, tp = confusion_matrix(prediction, y_test).ravel()
+print("Accuracy:", (np.mean(prediction == y_test)))
+print("Misclassification Rate:", ((fp + fn) / (tn + fp + fn + tp)))
+print("True Positive Rate :", (tp / (tp + fn)))
+print("False Positive Rate:", (fp / (tn + fp)))
+print("True Negative Rate:", (tn / (tn + fp)))
+print("Precision:", (tp / (tp + fp)))
+print("Prevalence:", ((tp + fn) / (tn + fp + fn + tp)))
--- Module 2/readme.md
+++ Module 2/readme.md
@@ -1,7 +1,8 @@
 # To run this, you need to install:
+**Python Console**
 python -m pip install --upgrade pip
 pip install numpy, pandas, matplotlib, scikit-learn
 
-**The use of scikit-learn its just to split the dataset**
+**The use of scikit-learn its just to split the dataset and scaling the dataset**
 
 **To run: ** python3 .\LearningImplementation.py
