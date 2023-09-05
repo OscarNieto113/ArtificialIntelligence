d9eee7aead9240a8dbd6213a8fdc6ed2aafc95b5
OscarNieto113 <a01705090@itesm.mx>
2023-09-05 08:06:45 -0600

    Lab 3 completed

---
+++ Module 4/lab3/lab3.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from keras.models import load_model
+from keras.preprocessing import image
+
+app = Flask(__name__)
+
+dic = {0 : 'Cat', 1 : 'Dog'}
+
+model = load_model('model.h5')
+
+model.make_predict_function()
+
+def predict_label(img_path):
+    i = image.load_img(img_path, target_size=(100,100))
+    i = image.img_to_array(i)/255.0
+    i = i.reshape(1, 100,100,3)
+
+    p = model.predict(i)
+    prediction_value = p[0][0]
+
+    if prediction_value > 0.85 :
+        return dic[1]
+    elif prediction_value < 0.15:
+        return dic[0]
+    else:
+        return prediction_value
+
+# routes
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    return render_template("index.html")
+
+@app.route("/submit", methods = ['POST'])
+def get_output():
+    if request.method == 'POST':
+        img = request.files['my_image']
+        img_path = "static/" + img.filename
+        img.save(img_path)
+        p = predict_label(img_path)
+    return render_template("index.html", prediction = p, img_path = img_path)
+
+if __name__ =='__main__':
+    #app.debug = True
+    app.run(debug = True)
